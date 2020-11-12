29106642611186e33f5d03f75dd85484f2fbe1bb
Clement Francis <clement.francis@epfl.ch>
2020-11-13 00:03:35 +0100

    First GPR Model

--- basic_estimator_model.py
+++ basic_estimator_model.py
@@ -1,13 +1,25 @@
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel
+
 class EstimatorModelBase:
-    def __init__(self):
-        pass
+    def __init__(self, kernel):
+        self.kernel = kernel
+        self.gpr = GaussianProcessRegressor(kernel)
+        
     def fit(self,X,y):
-        pass
+        return self.gpr.fit(X,y)
+    
     def predict(self,X):
         pass
-
+    
+    def optimal_hyperparameters(self):
+        return self.kernel.theta
+    
     def _predict_fX(self, X):
-        pass
+        
+        self.fX = self.gpr.predict(X)
+        
 
-    def _predict_VXt(self, X, t):
+    def _predict_VXt(self, X, t, aplha, betha, d):
         pass
+        
