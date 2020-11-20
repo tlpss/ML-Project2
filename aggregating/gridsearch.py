aaad6f28f9b2ea5081961cb768ebd8c3d833be51
tlpss <thomas17.lips@gmail.com>
2020-11-20 09:36:39 +0100

    pasting & bagging; catch out of memory errros

--- aggregating/gridsearch.py
+++ aggregating/gridsearch.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import threading
 from sklearn import clone 
+import traceback
 
 from aggregating.utils import flatten_X, normalized_error_VT
 
@@ -23,30 +24,35 @@ def evaluate_model(base_model,hyperparams, X_train, y_train,d, DeltaT,trials,N_t
     - normalized_error_VT
     - Flatten_Training_Sample
     """
-    print(f" {hyperparams} -> thread id = {threading.current_thread().ident}")
-    model = clone(base_model)
-    for k,v in hyperparams.items():
-        setattr(model,k,v)
-    model.fit(X_train,y_train)
-
     errors  = []
-    for trial in range(trials):
-        s_test = samples_generator(N_test, d, DeltaT)
-        s_test.generate_samples()
-        y_test = s_test.y
-        X_test = s_test.X
-        S_test = s_test.S
+    try:
+        print(f" {hyperparams} -> thread id = {threading.current_thread().ident}")
+        model = clone(base_model)
+        for k,v in hyperparams.items():
+            setattr(model,k,v)
+        model.fit(X_train,y_train)
 
-        Flattened_X_test = flatten_X(X_test)
+        for trial in range(trials):
+            s_test = samples_generator(N_test, d, DeltaT)
+            s_test.generate_samples()
+            y_test = s_test.y
+            X_test = s_test.X
+            S_test = s_test.S
 
-        V_T = y_test  
-        V_0 = s_test.generate_true_V(0)
-        V_0= V_0.mean()
-        y_hat = model.predict(Flattened_X_test)
+            Flattened_X_test = flatten_X(X_test)
 
-        error = normalized_error_VT(y_hat,V_T,V_0).item()
-        print(f"{hyperparams} , {trial} -> {error}")
-        errors.append(error)
-        print(errors)
-    print(f"{hyperparams} -> {errors}")
-    return errors
+            V_T = y_test  
+            V_0 = s_test.generate_true_V(0)
+            V_0= V_0.mean()
+            y_hat = model.predict(Flattened_X_test)
+
+            error = normalized_error_VT(y_hat,V_T,V_0).item()
+            print(f"{hyperparams} , {trial} -> {error}")
+            errors.append(error)
+        print(f"{hyperparams} -> {errors}")
+        return errors
+
+    except Exception as e:
+        print(hyperparams)
+        traceback.print_exc()
+        return None
