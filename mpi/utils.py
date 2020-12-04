c74a5a9da95947692df8f8f1dcb24c89f4ba5ceb
tlpss <thomas17.lips@gmail.com>
2020-12-04 13:35:46 +0100

    logging format update

--- mpi/utils.py
+++ mpi/utils.py
@@ -2,6 +2,8 @@ import logging
 from mpi4py import MPI 
 
 def generate_logger_MPI(logfile, level):
-    logging.basicConfig(filename=logfile, level=level)
+    logging.basicConfig(filename=logfile, level=level,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(name)s - %(funcName)s : %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',)
     logger = logging.getLogger("rank%i" % MPI.COMM_WORLD.Get_rank())
     return logger
